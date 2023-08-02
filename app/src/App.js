fac333ca7a10de0794a3292a66c36d552b03474e
jatannoury-Tradias <j.tannoury.ext@bankhaus-scheich.de>
2023-08-02 15:10:14 +0300

    ADD | Adding routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,22 @@
-import SignUp from './screens/SignUp';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
-    <div className="App">
-      <SignUp/>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/login" element={<SignIn />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
--- app/src/screens/SignUp.jsx
+++ app/src/screens/SignUp.jsx
@@ -1,10 +1,12 @@
-import "../styles/signUp.css"
+import "../styles/signUp.css";
 import SignUpHeader from "../components/SignUpHeader";
 import SignUpInput from "../components/SignUpInput";
+import { toast } from "react-toastify";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 const SignUp = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -13,23 +15,29 @@ const SignUp = () => {
     email: "",
     password: "",
   });
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   axiosInstance.register(formData).then((res) => {
-  //     if (res === 201) {
-  //       toast.success("User Registered");
-  //       setTimeout(() => {
-  //         navigate("/signIn");
-  //       }, 1000);
-  //     } else {
-  //       console.log("TOAST");
-  //       toast.error("Invalid Inputs");
-  //     }
-  //   });
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    toast.success("User Registered", {
+      autoClose: 1000,
+    });
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
+    // axiosInstance.register(formData).then((res) => {
+    //   if (res === 201) {
+    // toast.success("User Registered");
+    // setTimeout(() => {
+    //   navigate("/signIn");
+    // }, 1000);
+    //   } else {
+    //     console.log("TOAST");
+    //     toast.error("Invalid Inputs");
+    //   }
+    // });
+  };
   return (
     <div className="signup_container">
-      <form className="form_inputs">
+      <form className="form_inputs" onSubmit={handleSubmit}>
         <SignUpHeader />
         <div className="row">
           <SignUpInput
@@ -73,6 +81,6 @@ const SignUp = () => {
       </form>
     </div>
   );
-}
+};
 
-export default SignUp
+export default SignUp;
